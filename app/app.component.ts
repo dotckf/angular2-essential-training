722745f7a2946d8f50778e86755d1e4882357068
FAILING <DOTCKF@ad.dot.state.wi.us>
2019-06-11 13:18:34 -0500

    done with lesson 2

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,4 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: 'app/app.component.html',
   styleUrls: ['app/app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  //object literal sample data
+  firstMediaItem = {
+    id: 1,
+    name: "Firebug",
+    medium: "Series",
+    category: "Science Fiction",
+    year: 2010,
+    watchedOn: 1294166565384,
+    isFavorite: false
+  };
+
+  onMediaItemDelete(mediaItem) {
+    
+  }
+}
--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
   templateUrl: 'app/media-item.component.html',
   styleUrls: ['app/media-item.component.css']
 })
-export class MediaItemComponent { }
+export class MediaItemComponent {
+  @Input() mediaItem; //recommended not to use aliases
+  @Output() delete = new EventEmitter(); //recommended not to use aliases
+  /*name = "The Redemption";
+
+  wasWatched() {
+    return true;
+  }*/
+
+  onDelete() {
+    this.delete.emit(this.mediaItem);
+    //console.log("TODO: Implement - Delete has been clicked.");
+  }
+ }
